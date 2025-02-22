577eeac4b63f7cae0dac286be38b4d878d5f8829
DaeunSon <sn15dueo@naver.com>
2025-02-22 21:26:57 +0900

    Add : CustomRandomUtil 추가, 랜덤 초대 코드 생성 기능 구현

---
+++ src/main/java/com/alom/dorundorunbe/global/util/CustomRandomUtil.java
@@ -0,0 +1,16 @@
+package com.alom.dorundorunbe.global.util;
+
+import java.security.SecureRandom;
+
+public class CustomRandomUtil {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final SecureRandom RANDOM = new SecureRandom();
+
+    public static String generateRandomCode(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(CHARACTERS.charAt(RANDOM.nextInt(CHARACTERS.length())));
+        }
+        return sb.toString();
+    }
+}
