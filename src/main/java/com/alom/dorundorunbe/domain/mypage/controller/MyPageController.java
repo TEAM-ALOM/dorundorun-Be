3dd433b43ea080b529caa4b155b1fc937bd52ecc
JaeSangJang <dukejang01@gmail.com>
2025-03-12 17:39:29 +0900

    mypage service fixed

--- src/main/java/com/alom/dorundorunbe/domain/mypage/controller/MyPageController.java
+++ src/main/java/com/alom/dorundorunbe/domain/mypage/controller/MyPageController.java
@@ -3,45 +3,34 @@ package com.alom.dorundorunbe.domain.mypage.controller;
 
 import com.alom.dorundorunbe.domain.mypage.dto.*;
 import com.alom.dorundorunbe.domain.mypage.service.MyPageService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/myPage")
+@Tag(name = "마이페이지 API")
 public class MyPageController implements MyPageControllerSwaggerDocs{
 
     private final MyPageService myPageService;
 
-    @GetMapping("/")
-    public ResponseEntity<MyPageResponseDto> myPage(){
-        // 사용자 식별 - email 반환
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-
-        List<AchievementResponse> achievementResponses = myPageService.getAchievements(username);
-        String rank = myPageService.getUserRank(username);
-        List<MyPageRunningRecordResponse> runningRecords = myPageService.getRunningRecords(username);
-        String nickname = myPageService.getUserNickname(username);
-
-        MyPageResponseDto myPageResponseDto = new MyPageResponseDto(username, nickname, achievementResponses, rank, runningRecords);
-        return new ResponseEntity<>(myPageResponseDto, HttpStatus.OK);
+    @GetMapping("/{userId}")
+    public ResponseEntity<MyPageResponseDto> myPage(@PathVariable Long userId){
+        return ResponseEntity.status(HttpStatus.CREATED).body(myPageService.getMyPage(userId));
     }
 
-    @PutMapping("/update")
-    public ResponseEntity<String> updateUser(@RequestBody UserUpdateDto userDTO){
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        return myPageService.updateByEmail(userDTO, username);
+    @PutMapping("/update/{userId}")
+    public ResponseEntity<String> updateUser(@RequestBody UserUpdateDto userDTO, @PathVariable Long userId){
+        return myPageService.updateByEmail(userDTO, userId);
     }
 
-    @DeleteMapping("/delete")
-    public ResponseEntity<String> deleteUser() {
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        return myPageService.deleteUser(username);
+    @DeleteMapping("/delete/{userId}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long userId) {
+        return myPageService.deleteUser(userId);
     }
 
 }
--- src/main/java/com/alom/dorundorunbe/domain/mypage/controller/MyPageControllerSwaggerDocs.java
+++ src/main/java/com/alom/dorundorunbe/domain/mypage/controller/MyPageControllerSwaggerDocs.java
@@ -4,6 +4,7 @@ import com.alom.dorundorunbe.domain.mypage.dto.MyPageResponseDto;
 import com.alom.dorundorunbe.domain.mypage.dto.UserUpdateDto;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public interface MyPageControllerSwaggerDocs {
@@ -14,13 +15,17 @@ public interface MyPageControllerSwaggerDocs {
                     
                     마이페이지를 조회합니다
                     
-                    **반환 값**
+                    **입력 파라미터:**
+                    
+                    - 'Long id' : 유저 id
+                    
+                    **반환 값:**
                     
                     - 'MyPageResponseDto' : 마이페이지 dto
                     
                     """
     )
-    public ResponseEntity<MyPageResponseDto> myPage();
+    public ResponseEntity<MyPageResponseDto> myPage(@PathVariable Long id);
 
     @Operation(
             summary = "유저 정보 업데이트",
@@ -29,17 +34,17 @@ public interface MyPageControllerSwaggerDocs {
                     
                     유저 정보를 업데이트 합니다
                     
-                    **입력 파라미터**
+                    **입력 파라미터:**
                     
                     - 'String nickname' : 바뀐 닉네임
                     
-                    **반환 값**
+                    **반환 값:**
                     
                     - 'String response body' : 유저 정보 변경 성공 여부
                     
                     """
     )
-    public ResponseEntity<String> updateUser(@RequestBody UserUpdateDto userUpdateDTO);
+    public ResponseEntity<String> updateUser(@RequestBody UserUpdateDto userUpdateDTO, @PathVariable Long id);
 
     @Operation(
             summary = "",
@@ -48,13 +53,13 @@ public interface MyPageControllerSwaggerDocs {
                     
                     유저를 삭제합니다
                     
-                    **반환 값**
+                    **반환 값:**
                     
                     - 'String response body' : 유저 삭제 성공 여부
                     
                     """
     )
-    public ResponseEntity<String> deleteUser();
+    public ResponseEntity<String> deleteUser(@PathVariable Long id);
 
 
 }
