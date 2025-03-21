ead9c0c2e6d08a5077ed0b469d3e57190f2a6507
JaeSangJang <dukejang01@gmail.com>
2025-03-21 20:31:09 +0900

    error handling modified

--- src/main/java/com/alom/dorundorunbe/domain/mypage/service/MyPageService.java
+++ src/main/java/com/alom/dorundorunbe/domain/mypage/service/MyPageService.java
@@ -1,37 +1,40 @@
 package com.alom.dorundorunbe.domain.mypage.service;
 
-import com.alom.dorundorunbe.domain.achievement.repository.UserAchievementRepository;
 import com.alom.dorundorunbe.domain.achievement.service.AchievementService;
 import com.alom.dorundorunbe.domain.item.service.ItemService;
 import com.alom.dorundorunbe.domain.mypage.dto.MyPageResponseDto;
-import com.alom.dorundorunbe.domain.runningrecord.repository.RunningRecordRepository;
 import com.alom.dorundorunbe.domain.runningrecord.service.RunningRecordService;
 import com.alom.dorundorunbe.domain.user.domain.User;
 import com.alom.dorundorunbe.domain.mypage.dto.UserUpdateDto;
+import com.alom.dorundorunbe.domain.user.repository.UserRepository;
 import com.alom.dorundorunbe.domain.user.service.UserService;
+import com.alom.dorundorunbe.global.exception.BusinessException;
+import com.alom.dorundorunbe.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
 @RequiredArgsConstructor
 public class MyPageService {
     private final UserService userService;
-    private final RunningRecordRepository runningRecordRepository;
-    private final UserAchievementRepository userAchievementRepository;
     private final ItemService itemService;
     private final RunningRecordService runningRecordService;
     private final AchievementService achievementService;
+    private final UserRepository userRepository;
 
+    @Transactional
     public MyPageResponseDto getMyPage(Long userId){
         User user = userService.findById(userId);
-        Pageable runningPage = PageRequest.of(0, 10, Sort.by("endTime").descending());
-        Pageable achievementPage = PageRequest.of(0, 10, Sort.by("id").descending());
+        Pageable runningPage = PageRequest.of(0, 5, Sort.by("endTime").descending());
+        Pageable achievementPage = PageRequest.of(0, 5, Sort.by("id").descending());
         MyPageResponseDto myPageResponseDto = MyPageResponseDto.builder()
                 .email(user.getEmail())
                 .rank(user.getRanking())
@@ -45,22 +48,22 @@ public class MyPageService {
     }
 
 
-    public boolean checkNickNameDuplicate(String nickName) {
-        return userService.existsByNickname(nickName);
-    }
-
+    @Transactional
     public ResponseEntity<String> updateByEmail(UserUpdateDto userDTO, Long userId) {
         User user = userService.findById(userId);
         if(userDTO.getNickname() == null)
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Nickname is required");
-        if(checkNickNameDuplicate(userDTO.getNickname()))
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Nickname already exists");
+            throw new BusinessException(ErrorCode.EMPTY_NICKNAME);
+        if (userRepository.existsByNickname(userDTO.getNickname())) {
+            throw new BusinessException(ErrorCode.NICKNAME_DUPLICATE);
+        }
+
         user.setNickname(userDTO.getNickname());
         userService.save(user);
         return ResponseEntity.status(HttpStatus.OK).body("User updated successfully");
     }
 
     // soft delete -> refresh 토큰 삭제 로직 추가 필요
+    @Transactional
     public ResponseEntity<String> deleteUser(Long userId) {
         User user = userService.findById(userId);
         userService.delete(user);
